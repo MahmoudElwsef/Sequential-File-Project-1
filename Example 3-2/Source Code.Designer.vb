cd327882af263f41d69d3ebacf2297f996120f11
MahmoudElwsef <mahmoud.elwsef.0.0@gmail.com>
2020-07-06 05:49:42 -0700

    initial commit

---
+++ Example 3-2/Source Code.Designer.vb
@@ -0,0 +1,257 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Tahoma", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(891, 59)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(135, 29)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "رقم الصنف"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Tahoma", 13.8!, System.Drawing.FontStyle.Bold)
+        Me.Label2.Location = New System.Drawing.Point(325, 64)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(172, 29)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "اسـم الـصـنـف"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Tahoma", 13.8!, System.Drawing.FontStyle.Bold)
+        Me.Label3.Location = New System.Drawing.Point(891, 128)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(136, 29)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "الوحـــــــده"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Tahoma", 13.8!, System.Drawing.FontStyle.Bold)
+        Me.Label4.Location = New System.Drawing.Point(322, 133)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(175, 29)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "الرصيد الـحالي"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Tahoma", 13.8!, System.Drawing.FontStyle.Bold)
+        Me.Label5.Location = New System.Drawing.Point(891, 205)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(135, 29)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "سعر البـيـع"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Tahoma", 13.8!, System.Drawing.FontStyle.Bold)
+        Me.Label6.Location = New System.Drawing.Point(322, 210)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(174, 29)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "سـعر الــجمـله"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Tahoma", 13.8!, System.Drawing.FontStyle.Bold)
+        Me.Button1.Location = New System.Drawing.Point(858, 580)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(168, 46)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "صنف جديد"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Tahoma", 13.8!, System.Drawing.FontStyle.Bold)
+        Me.Button2.Location = New System.Drawing.Point(659, 580)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(168, 46)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "عرض الصنف"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Tahoma", 13.8!, System.Drawing.FontStyle.Bold)
+        Me.Button3.Location = New System.Drawing.Point(445, 580)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(168, 46)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "تعديل"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("Tahoma", 13.8!, System.Drawing.FontStyle.Bold)
+        Me.Button4.Location = New System.Drawing.Point(235, 580)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(168, 46)
+        Me.Button4.TabIndex = 9
+        Me.Button4.Text = "حذف صنف"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Font = New System.Drawing.Font("Tahoma", 13.8!, System.Drawing.FontStyle.Bold)
+        Me.Button5.Location = New System.Drawing.Point(12, 580)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(168, 46)
+        Me.Button5.TabIndex = 10
+        Me.Button5.Text = "خروج"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(616, 64)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(260, 24)
+        Me.TextBox1.TabIndex = 11
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(52, 64)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(260, 24)
+        Me.TextBox2.TabIndex = 12
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(616, 133)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(260, 24)
+        Me.TextBox3.TabIndex = 13
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(52, 133)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(260, 24)
+        Me.TextBox4.TabIndex = 14
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(616, 210)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(260, 24)
+        Me.TextBox5.TabIndex = 15
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(52, 210)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(260, 24)
+        Me.TextBox6.TabIndex = 16
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Location = New System.Drawing.Point(52, 264)
+        Me.ListBox1.MultiColumn = True
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.ScrollAlwaysVisible = True
+        Me.ListBox1.Size = New System.Drawing.Size(824, 292)
+        Me.ListBox1.TabIndex = 17
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(1038, 674)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents ListBox1 As ListBox
+End Class
---
+++ Example 3-2/Source Code.vb
@@ -0,0 +1,210 @@
+﻿Imports System.IO
+Imports System.IO.IOException
+
+Public Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Declare All Variables in Program
+    Dim position, num, unit, money_now, sale_price, total_price As Integer
+    Dim name As String
+
+    'Proucdure To Clear All TextBoxes
+    Sub clear()
+        TextBox1.Clear()
+        TextBox2.Clear()
+        TextBox3.Clear()
+        TextBox4.Clear()
+        TextBox5.Clear()
+        TextBox6.Clear()
+        TextBox1.Focus()
+    End Sub
+
+    'Proucdure To Display Data in ListBox
+    Sub view()
+
+        FileOpen(1, "xx.txt", OpenMode.Input)
+
+        ListBox1.Height = (ListBox1.ItemHeight + 3) * 12
+        ListBox1.Items.Clear() 'لمسح البيانات الموجوده في الليست بوكس
+
+        ListBox1.Items.Add("") 'علشان نسيب اول سطر ف الليست بوكس فاضي ويبدأ يكتب من السطر التاني
+        ListBox1.Items.Add("|" + "رقم الصف")
+        ListBox1.Items.Add("|" + "--------------------------------------------------------------------------")
+        ListBox1.Items.Add("|" + "اسم الصنف")
+        ListBox1.Items.Add("|" + "--------------------------------------------------------------------------")
+        ListBox1.Items.Add("|" + "الوحده")
+        ListBox1.Items.Add("|" + "--------------------------------------------------------------------------")
+        ListBox1.Items.Add("|" + "الرصيد الحالي")
+        ListBox1.Items.Add("|" + "--------------------------------------------------------------------------")
+        ListBox1.Items.Add("|" + "السعر")
+        ListBox1.Items.Add("|" + "--------------------------------------------------------------------------")
+        ListBox1.Items.Add("|" + " السعر الكلي")
+
+        Do While Not EOF(1)
+
+            'بنجيب بيانات أول صف ف الفايل
+            Input(1, num)
+            Input(1, name)
+            Input(1, unit)
+            Input(1, money_now)
+            Input(1, sale_price)
+            Input(1, total_price)
+
+            'بنعرض البيانات في الليست بوكس
+            ListBox1.Items.Add("")
+            ListBox1.Items.Add("|" + Str(num))
+            ListBox1.Items.Add("|" + "--------------------------------------------------------------------------")
+            ListBox1.Items.Add("|" + name)
+            ListBox1.Items.Add("|" + "--------------------------------------------------------------------------")
+            ListBox1.Items.Add("|" + Str(unit))
+            ListBox1.Items.Add("|" + "--------------------------------------------------------------------------")
+            ListBox1.Items.Add("|" + Str(money_now))
+            ListBox1.Items.Add("|" + "--------------------------------------------------------------------------")
+            ListBox1.Items.Add("|" + Str(sale_price))
+            ListBox1.Items.Add("|" + "--------------------------------------------------------------------------")
+            ListBox1.Items.Add("|" + Str(total_price))
+
+            'علشان لما اكتب رقم الصنف واضغط علي عرض الصنف يعرضلي كل البيانات بتاعته ف كل التيكست بوكسيز
+            If num = Val(TextBox1.Text) Then
+                TextBox2.Text = name
+                TextBox3.Text = unit
+                TextBox4.Text = money_now
+                TextBox5.Text = sale_price
+                TextBox6.Text = total_price
+            End If
+
+        Loop
+
+        FileClose(1)
+
+    End Sub
+
+    'Exit Button
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        End
+    End Sub
+
+    'View Button
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        view()
+    End Sub
+
+    'Add Button
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        FileOpen(1, "XX.txt", OpenMode.Append)
+
+        num = Val(TextBox1.Text)
+        name = TextBox2.Text
+        unit = Val(TextBox3.Text)
+        money_now = Val(TextBox4.Text)
+        sale_price = Val(TextBox5.Text)
+        total_price = Val(TextBox6.Text)
+
+        WriteLine(1, num, name, unit, money_now, sale_price, total_price)
+
+        FileClose(1)
+
+        MsgBox("تم حفظ الصنف بنجاح")
+        clear()
+
+    End Sub
+
+    'Edit Button
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        FileOpen(1, "XX.txt", OpenMode.Input)
+        FileOpen(2, "YY.txt", OpenMode.Append)
+
+        Do While Not EOF(1)
+
+            Input(1, num)
+            Input(1, name)
+            Input(1, unit)
+            Input(1, money_now)
+            Input(1, sale_price)
+            Input(1, total_price)
+
+            'IF this are The Record You want To Edit
+            If num = Val(TextBox1.Text) Then
+                position = Loc(1)
+
+                name = TextBox2.Text
+                unit = Val(TextBox3.Text)
+                money_now = Val(TextBox4.Text)
+                sale_price = Val(TextBox5.Text)
+                total_price = Val(TextBox6.Text)
+
+                If (Loc(1) <> position - 1) Then
+                    WriteLine(2, num, name, unit, money_now, sale_price, total_price)
+                End If
+            End If
+
+            'IF this are Not The Record You want To Edit
+            If num <> Val(TextBox1.Text) Then
+                position = Loc(1)
+                If (Loc(1) <> position - 1) Then
+                    WriteLine(2, num, name, unit, money_now, sale_price, total_price)
+                End If
+            End If
+
+        Loop
+
+        FileClose(2)
+        FileClose(1)
+
+        Kill("XX.txt")
+        Rename("YY.txt", "XX.txt")
+        clear()
+    End Sub
+
+    'Delete Button
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+        FileOpen(1, "XX.txt", OpenMode.Input)
+        FileOpen(2, "YY.txt", OpenMode.Append)
+
+        Do While Not EOF(1)
+
+            Input(1, num)
+            Input(1, name)
+            Input(1, unit)
+            Input(1, money_now)
+            Input(1, sale_price)
+            Input(1, total_price)
+
+            'IF this are Not The Record You want To Delete
+            If num <> Val(TextBox1.Text) Then
+
+                position = Loc(1)
+                If (Loc(1) <> position - 1) Then
+                    WriteLine(2, num, name, unit, money_now, sale_price, total_price)
+                End If
+            End If
+
+        Loop
+
+        FileClose(2)
+        FileClose(1)
+
+        Kill("XX.txt")
+        Rename("YY.txt", "XX.txt")
+        clear()
+
+    End Sub
+
+    'استحدمنا حدث آخر لزرار اضافه عنصر جديد علشان بمجرد م اضغط عليه واضيف عنصر جديدالتعديل يظهر ف الليست بوكس
+    Private Sub Button1_MouseUp(sender As Object, e As MouseEventArgs) Handles Button1.MouseUp
+        view()
+    End Sub
+
+    'استحدمنا حدث آخر لزرار التعديل علشان بمجرد م اضغط عليه واعدل التعديل يظهر ف الليست بوكس
+    Private Sub Button3_MouseUp(sender As Object, e As MouseEventArgs) Handles Button3.MouseUp
+        view()
+    End Sub
+
+    'استحدمنا حدث آخر لزرار حذف عنصر علشان بمجرد م اضغط عليه واحذف عنصر التعديل يظهر ف الليست بوكس
+    Private Sub Button4_MouseUp(sender As Object, e As MouseEventArgs) Handles Button4.MouseUp
+        view()
+    End Sub
+
+End Class
